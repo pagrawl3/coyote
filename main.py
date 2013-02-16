7eb9877fd49778a923293dfe5c410911db70f1d6
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-02-16 07:10:02 -0600

    updating

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cleaning
 import file_directory
 import json
+import os
+from sys import argv
+script_name, filename = argv
+
 def get_data(filename):
     return json.dumps({'files':file_directory.get_files(),'errors':cleaning.parseFile(filename)})
 
-def parseFile(readFilename='output.txt',writeFilename='json.txt'):
+def parseFile(readFilename='output.txt',writeFilename='json.json'):
     f = open(writeFilename,'wb')
     f.write(get_data(readFilename))
     f.close()
@@ -15,3 +19,16 @@ def createFiles(filename):
         f = open(w['path'],'wb')
         f.write(w['content'])
         f.close()
+
+
+parseFile(filename)
+
+jtime = os.stat('json.json').st_mtime
+
+while(1):
+    newjtime = os.stat('json.json').st_mtime
+    if (newjtime!=jtime):
+        createFiles(filename)
+        jtime = newjtime
+        
+        
