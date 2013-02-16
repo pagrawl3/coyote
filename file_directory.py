d7b0a5ced5db81aff7eee1a87c633da356c979d5
Rishabh Marya <rishabhmarya@Rishabhs-MacBook-Pro.local>
2013-02-16 02:58:19 -0600

    got directory listings

--- file_directory.py
+++ file_directory.py
@@ -1,14 +1,17 @@
 import os
 
-for dirname, dirnames, filenames in os.walk('./mp2'):
+for dirname, dirnames, filenames in os.walk('.'):
     # print path to all subdirectories first.
+    listing = []
     for subdirname in dirnames:
-        print os.path.join(dirname, subdirname)
-    print
-    print
-    print
+        listing.append(os.path.join(dirname, subdirname))
     # print path to all filenames.
     for filename in filenames:
-        print os.path.join(dirname, filename)
+        listing.append(os.path.join(dirname, filename))
+
+    final_listing = [w for w in listing if ((".h" in w[-2:] or ".cpp" in w[-4:]) and 'svn-base' not in w)]
+    for a in final_listing:
+        #if len(a)>0:
+        print a
 
     
