d09c57f404f389c9ca709cf9440ed1684225b579
Matthew Wesly <wesly1@illinois.edu>
2013-02-16 05:59:55 -0600

    fixed function name search

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import file_directory
 import json
 def get_data(filename):
     return json.dumps({'files':file_directory.get_files(),'errors':cleaning.parseFile(filename)})
+
+def parseFile(readFilename='output.txt',writeFilename='json.txt'):
+    f = open(writeFilename,'wb')
+    f.write(get_data(readFilename))
+    f.close()
+    
