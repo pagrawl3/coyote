334ec09fd8b27fb1588ceacf1a818063d1851e9b
Pratham Agrawal <pratham.agrawal92@gmail.com>
2013-02-15 23:38:08 -0600

    Added some of the stylings

--- web/js/index-ck.js
+++ web/js/index-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){alert("Hello World!")});
+$(document).ready(function(){var e=ace.edit("editor");e.getSession().setMode("ace/mode/c_cpp")});
--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	alert("Hello World!");
+	// alert("Hello World!");
+
+	var editor = ace.edit("editor");
+    //editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/c_cpp");
+    //editor.getSession().setUseWrapMode(true);
 });
