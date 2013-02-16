221cfbc0922226a43e3fa7d0a451f4e1d70778c6
Matthew Wesly <wesly1@illinois.edu>
2013-02-15 21:49:42 -0600

    more python code

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,4 @@
+
 def trim(a):
     return a.lstrip().rstrip()
 
@@ -5,11 +6,38 @@ def cleanLine(line):
 	return ':'.join(line.split(':')[1:])
 
 def parseLine(line):
-    clean = cleanLine(line)
-    temp = clean.split('(')
-    first = '('.join(temp[:-1])
-    last = '(' + temp[-1]
-    middle = last.split(':')[0][1:]
-    third = last.split(':')[1][:-1]
-    first = trim(first)
-    return first,middle, third
+    if '.cpp' in line:
+        clean = cleanLine(line)
+        temp = clean.split('(')
+        first = '('.join(temp[:-1])
+        last = '(' + temp[-1]
+        middle = last.split(':')[0][1:]
+        third = last.split(':')[1][:-1]
+        first = trim(first)
+        return first,middle, third
+    return trim(cleanLine(line))
+
+def getSplitVal(raw):
+    for w in raw.split('\n'):
+        if w.rstrip()[-2:]=="==":
+            return w + '\n'
+
+def test():
+    filename = 'output.txt'
+    raw = open(filename,'r').read()
+    splitVal = getSplitVal(raw)
+    data = raw.split(splitVal)[1:]
+    splitVal = splitVal[:-1]
+    return parseError(data[0].replace(splitVal,''))
+    return [parseError(w.replace(splitVal,'')) for w in data]
+
+def parseError(chunk):
+    data = chunk.split('\n')
+    if len(data) >= 2:
+        error = data[1]
+        trace = getTrace(data[2:])
+        return error,trace
+    return None
+
+def getTrace(data_chunk):
+    return [parseLine(w) for w in data_chunk if w]
